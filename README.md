044c16b97f0b70837fad4d9361fd2dfe387b7ccb
ranuzz <ranuzz@outlook.com>
2022-02-06 14:40:47 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-6njsnh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-6njsnh)
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css';
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = `
+  <h1 class="text-emerald-500 text-center">Hello Vite!</h1>
+`;
