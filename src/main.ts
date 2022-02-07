089e5a12a2ff9638e0c8c006faeec00bdb0f1e96
ranuzz <ranuzz@outlook.com>
2022-02-06 17:53:12 -0800

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,5 @@ import './style.css';
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
-  <h1 class="text-emerald-500 text-center">Hello Vite!</h1>
+  <h1 class="text-emerald-500 text-center">Hello Vite with Tailwind CSS!</h1>
 `;
